4dd90eb84a3a20cea9191537291a0a2a096d8363
Sana Ishtiaq <sanaaishtiaq007@gmail.com>
2026-05-29 05:20:18 +0500

    lets gooo

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'screens/generator_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
